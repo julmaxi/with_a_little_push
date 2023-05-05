601042e2087ccb9dd27586036a09c74c9468305f
Julius Steen <steen@cl.uni-heidelberg.de>
2023-05-05 16:00:16 +0200

    Initial commit

---
+++ nlifactspush/augment_dataset.py
@@ -0,0 +1,112 @@
+import datasets
+import random
+from collections import defaultdict
+
+from typing import Callable, Any
+from transformers import BartForConditionalGeneration, AutoTokenizer
+import torch
+
+
+def batched_instance_fn(f: Callable[
+    [dict[str, Any]],
+    list[dict[str, Any]]
+]):
+    def inner(instances: dict[str, list], **kwargs) -> dict[str, list]:
+        keys = list(instances.keys())
+        new_keys = ["sample_idx", "augment_type", "phrase_idx", "phrase_type"]
+        new_keys.extend(instances.keys())
+        n_instances = len(instances[keys[0]])
+        out_dict = {k: [] for k in new_keys}
+        for idx in range(n_instances):
+            d = {k: instances[k][idx] for k in keys}
+            out_dicts = f(d, **kwargs)
+            if len(out_dicts) == 0:
+                continue
+            for out in out_dicts:
+                for k in new_keys:
+                    out_dict[k].append(out[k])
+        return out_dict
+
+    return inner
+
+
+def augment_dataset(dataset: datasets.arrow_dataset.Dataset, ratio=1):
+    PHRASES = list(enumerate([
+        ("I am not sure, but ", "hedging"),
+        ("I am not sure but I do know that ", "hedging"),
+        ("I believe ",  "hedging"),
+        ("I do not have information on this but ", "hedging"),
+        ("I think ", "hedging"),
+        ("Here is what I know: ", "introduction"),
+        ("yep. Also ", "introduction"),
+        ("Sure! Here is what I know: ", "introduction"),
+        ("I love that! ", "opinion"),
+        ("I like that! ", "opinion")
+    ]))
+    
+    @batched_instance_fn
+    def phrase_augment_fn(instance: dict[str, Any], n_samples=1) -> dict[str, Any]:
+        hypothesis = instance["hypothesis"]
+        confounders = random.sample(PHRASES, k=n_samples)
+
+        result = []
+
+        for idx, (phrase_idx, (phrase, phrase_type)) in enumerate(confounders):
+            out = dict(instance)
+            out["augment_type"] = "dialog"
+            out["phrase_idx"] = phrase_idx
+            out["phrase_type"] = phrase_type
+            out["sample_idx"] = 1 + idx
+            sample = phrase + hypothesis[0].lower() + hypothesis[1:]
+            out["hypothesis"] = sample
+            result.append(out)
+        
+        return result
+
+    @batched_instance_fn
+    def original_fn(instance: dict[str, Any], **kwargs):
+        out = dict(instance)
+        out["augment_type"] = "original"
+        out["sample_idx"] = 0
+        out["phrase_idx"] = 0
+        out["phrase_type"] = ""
+    
+        return [out]
+
+    augment_functions = [original_fn]
+    augment_functions.append(
+        phrase_augment_fn
+    )
+
+    return datasets.concatenate_datasets([
+        dataset.map(
+            lambda x: f(x, n_samples=5),
+            batched=True,
+            load_from_cache_file=False,
+        ) for f in augment_functions
+    ])
+
+
+def main():
+    dataset = datasets.load_dataset("anli")
+    dataset = {key: augment_dataset(d) for key, d in dataset.items()}
+
+    dataset = datasets.dataset_dict.DatasetDict(
+        {
+            "train": datasets.concatenate_datasets(
+                [dataset["train_r1"], dataset["train_r2"], dataset["train_r3"]]
+            ),
+            "dev": datasets.concatenate_datasets(
+                [dataset["dev_r1"], dataset["dev_r2"], dataset["dev_r3"]]
+            ),
+            "test": datasets.concatenate_datasets(
+                [dataset["test_r1"], dataset["test_r2"], dataset["test_r3"]]
+            )
+        }
+    )
+
+    dataset.save_to_disk("anli-phrase-augmented")
+
+
+if __name__ == "__main__":
+    main()
---
+++ nlifactspush/train.py
@@ -0,0 +1,113 @@
+import argparse
+
+import numpy as np
+import torch
+from pathlib import Path
+
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, Trainer, TrainingArguments, DataCollatorWithPadding
+import datasets
+import random
+import os
+
+from datetime import datetime
+
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--dataset", default="anli-phrase-augmented")
+    parser.add_argument("-m", "--model", dest="base_model", default="MoritzLaurer/DeBERTa-v3-large-mnli-fever-anli-ling-wanli")
+    parser.add_argument("-r", "--report", dest="report", default=False, action="store_true")
+    parser.add_argument("--lr", dest="learning_rate", default=5e-06, type=float)
+    parser.add_argument("--aug", dest="augmentations", default=None, type=lambda x: x.split(","))
+    parser.add_argument("-s", "--steps", default=20000, type=int, help="Number of training steps")
+    parser.add_argument("--sample", default=None, type=int, help="Sample index to train on")
+    parser.add_argument("--sample-phrases", default=False, action="store_true")
+
+    args = parser.parse_args()
+
+    dataset = datasets.load_from_disk(args.dataset)
+
+    tokenizer = AutoTokenizer.from_pretrained(args.base_model)
+
+    def preprocess(entry):
+        out_dict = tokenizer(entry["premise"], entry["hypothesis"], truncation=True, max_length=512)
+        out_dict["label"] = entry["label"]
+        return out_dict
+
+    aug_str = "all"
+    if args.augmentations is not None:
+        aug_str = "+".join(args.augmentations)
+    
+    if args.augmentations is not None:
+        dataset = dataset.filter(lambda x: x["augment_type"] in args.augmentations)
+
+    if args.sample_phrases:
+        all_phrase_indices = set()
+        for entry in dataset["train"]:
+            if entry["augment_type"] == "dialog":
+                all_phrase_indices.add(entry["phrase_idx"])
+
+        sampled_phrase_indices = random.sample(list(all_phrase_indices), len(all_phrase_indices) // 2)
+
+        aug_str += "_sampled"
+
+        dataset = dataset.filter(lambda x: x["augment_type"] != "dialog" or x["phrase_idx"] in sampled_phrase_indices)
+
+    dataset = dataset.map(preprocess, batched=False, remove_columns=list(dataset["train"].features))
+    collator = DataCollatorWithPadding(tokenizer=tokenizer)
+    lr = args.learning_rate
+    model = AutoModelForSequenceClassification.from_pretrained(args.base_model, num_labels=3)
+    model_name = f"{aug_str}-{lr}"
+
+    batch_size = 8
+    grad_acc = 4
+
+    job_id = os.environ.get('SLURM_JOB_ID')
+    if job_id is None:
+        job_id = datetime.today().strftime('%Y%m%d_%H%M%S')
+    saved_model_name = f"{model_name}_{job_id}"
+
+    training_args = TrainingArguments(
+        output_dir=f"nli_trainer_{saved_model_name}",
+        num_train_epochs= args.steps / (len(dataset["train"]) / (batch_size * grad_acc * torch.cuda.device_count())),
+        learning_rate=lr,
+        per_device_train_batch_size=batch_size,   
+        gradient_accumulation_steps=grad_acc,
+        per_device_eval_batch_size=64,
+        warmup_ratio=0.06,
+        weight_decay=0.01,
+        fp16=True,
+        report_to="wandb" if args.report else "none",
+        save_strategy="steps",
+        save_total_limit=10,
+        save_steps=5000,
+        logging_strategy="steps",
+        logging_steps=100,
+        evaluation_strategy="steps",
+        eval_steps=5000,
+        load_best_model_at_end=True
+    )
+
+    def compute_metrics(eval_results):
+        predicted = np.array(eval_results.predictions).argmax(axis=-1)
+        return {"accuracy": (eval_results.label_ids == predicted).mean()}
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=dataset["train"],
+        eval_dataset=dataset["dev"],
+        compute_metrics=compute_metrics,
+        data_collator=collator,
+    )
+    out_dir = Path("./nli_models")
+    out_dir.mkdir(exist_ok=True)
+
+    trainer.train()
+
+    trainer.save_model(out_dir / saved_model_name)
+
+
+if __name__ == "__main__":
+    main()
